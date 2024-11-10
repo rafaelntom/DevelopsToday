f550f2ad33ad54239fc4c5004ea69f8783065e0e
Rafael_Tomazini <rafaelnt83@gmail.com>
2024-11-10 16:53:01 -0300

    Initial project setup

---
+++ car_dealer/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div className="min-h-screen bg-slate-800 flex flex-col items-center justify-center font-sans">
+      Develops Today Assignment
+    </div>
+  );
+};
+
+export default HomePage;
